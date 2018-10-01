e2d974185c057d9fff0d4511a5c85aa2ec4051da
Sara Blevins <dvlnthebludrs@gmail.com>
2018-09-30 20:48:39 -0400

    Add Axios store venues in a state near KSC

--- reactmapsapp/src/App.js
+++ reactmapsapp/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
-
 import './App.css';
-
+import axios from 'axios';
 
 
 class App extends Component {
 
+  state = {
+    venues: []
+  }
+
   componentDidMount() {
+    this.getVenues()
     this.renderMap()
   }
 
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBQowO9QXcPdkhyyemu370I_16x62BtX8k&callback=initMap")
     window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: "IZB4KA3CRYVCJJYZF1MJGU23KJLOY1LTWKSDEH5YGGZLYQZQ",
+      client_secret: "3W5RLNDW2OKJJH4HH0CGI5SHMPQXMU0F3CN02ZHRQYOMD0FM",
+      query: "sights",
+      near: "Kennedy Space Center",
+      v: "20182507"
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      this.setState({
+        venues: response.data.response.groups[0].items
+      })
+    })
+      .catch(error => {
+        console.log("ERROR " + error)
+      })
   }
 
   initMap = () => {
