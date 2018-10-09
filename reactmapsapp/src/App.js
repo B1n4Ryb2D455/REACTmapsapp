910bbb3fe04f933e044131e8665f9e51ad1b74c8
Sara Blevins <dvlnthebludrs@gmail.com>
2018-10-09 18:51:11 -0400

    Add marker names on point

--- reactmapsapp/src/App.js
+++ reactmapsapp/src/App.js
@@ -2,77 +2,79 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-
 class App extends Component {
+	state = {
+		venues: []
+	};
 
-  state = {
-    venues: []
-  }
+	componentDidMount() {
+		this.getVenues();
+		// this.renderMap()
+	}
 
-  componentDidMount() {
-    this.getVenues()
-    // this.renderMap()
-  }
+	renderMap = () => {
+		loadScript(
+			'https://maps.googleapis.com/maps/api/js?key=AIzaSyBQowO9QXcPdkhyyemu370I_16x62BtX8k&callback=initMap'
+		);
+		window.initMap = this.initMap;
+	};
 
-  renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBQowO9QXcPdkhyyemu370I_16x62BtX8k&callback=initMap")
-    window.initMap = this.initMap
-  }
+	getVenues = () => {
+		const endPoint = 'https://api.foursquare.com/v2/venues/explore?';
+		const parameters = {
+			client_id: 'IZB4KA3CRYVCJJYZF1MJGU23KJLOY1LTWKSDEH5YGGZLYQZQ',
+			client_secret: '3W5RLNDW2OKJJH4HH0CGI5SHMPQXMU0F3CN02ZHRQYOMD0FM',
+			query: 'sights',
+			near: 'Kennedy Space Center',
+			v: '20182507'
+		};
 
-  getVenues = () => {
-    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
-    const parameters = {
-      client_id: "IZB4KA3CRYVCJJYZF1MJGU23KJLOY1LTWKSDEH5YGGZLYQZQ",
-      client_secret: "3W5RLNDW2OKJJH4HH0CGI5SHMPQXMU0F3CN02ZHRQYOMD0FM",
-      query: "sights",
-      near: "Kennedy Space Center",
-      v: "20182507"
-    }
+		axios
+			.get(endPoint + new URLSearchParams(parameters))
+			.then((response) => {
+				this.setState(
+					{
+						venues: response.data.response.groups[0].items
+					},
+					this.renderMap()
+				);
+			})
+			.catch((error) => {
+				console.log('ERROR ' + error);
+			});
+	};
 
-    axios.get(endPoint + new URLSearchParams(parameters))
-      .then(response => {
-        this.setState({
-          venues: response.data.response.groups[0].items
-        }, this.renderMap())
-      })
-      .catch(error => {
-        console.log("ERROR " + error)
-      })
-  }
+	initMap = () => {
+		const map = new window.google.maps.Map(document.getElementById('map'), {
+			center: { lat: 28.5728722, lng: -80.6489808 },
+			zoom: 10
+		});
 
-  initMap = () => {
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 28.5728722, lng: -80.6489808 },
-      zoom: 10
-    })
+		this.state.venues.map((eachVenue) => {
+			const marker = new window.google.maps.Marker({
+				position: { lat: eachVenue.venue.location.lat, lng: eachVenue.venue.location.lng },
+				map: map,
+				title: eachVenue.venue.name
+			});
+		});
+	};
 
-    this.state.venues.map(eachVenue => {
-
-    const marker = new window.google.maps.Marker ({
-      position: {lat: eachVenue.venue.location.lat, lng: eachVenue.venue.location.lng},
-    map: map,
-
-    })
-  })
-}
-
-render() {
-  return (
-    <main>
-      <div id="map"></div>
-    </main>
-
-  );
-}
+	render() {
+		return (
+			<main>
+				<div id="map" />
+			</main>
+		);
+	}
 }
 
 function loadScript(url) {
-  const index = window.document.getElementsByTagName("script")[0]
-  const script = window.document.createElement("script")
-  script.src = url
-  script.async = true
-  script.defer = true
-  index.parentNode.insertBefore(script, index)
+	const index = window.document.getElementsByTagName('script')[0];
+	const script = window.document.createElement('script');
+	script.src = url;
+	script.async = true;
+	script.defer = true;
+	index.parentNode.insertBefore(script, index);
 }
 
 export default App;
