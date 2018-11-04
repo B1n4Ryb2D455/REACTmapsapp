a65fcf64ee4f0a88b38ba6fcb362a864bfb73b08
Sara Blevins <dvlnthebludrs@gmail.com>
2018-11-04 13:31:55 -0500

    Make search input field accessible

--- reactmapsapp/src/Components/Sidebar.js
+++ reactmapsapp/src/Components/Sidebar.js
@@ -9,7 +9,12 @@ class sidebar extends Component {
     render() {
         return (
             <sidebar id="sidebar">
-                <input placeholder="Places to See" value={this.props.query} onChange={(e) => { this.props.filterVenues(e.target.value) }} />
+                <form>
+                    <label class="search-label" for="search"> Search: </label>
+                    <input id="search" type="text" placeholder="Places to See" value={this.props.query} onChange={(e) => { this.props.filterVenues(e.target.value) }} />
+                </form>
+
+
                 <br />
                 {
                     this.props.filteredVenues && this.props.filteredVenues.length > 0 && this.props.filteredVenues.map((venue, index) => (
