e1328bcee9c3d185a44a477e7620beddb4a90bf3
Sara Blevins <dvlnthebludrs@gmail.com>
2018-11-04 12:31:12 -0500

    Customize css

--- reactmapsapp/src/Components/Sidebar.js
+++ reactmapsapp/src/Components/Sidebar.js
@@ -9,7 +9,7 @@ class sidebar extends Component {
     render() {
         return (
             <sidebar id="sidebar">
-                <input placeholder="Filter Content" value={this.props.query} onChange={(e) => { this.props.filterVenues(e.target.value) }} />
+                <input placeholder="Places to See" value={this.props.query} onChange={(e) => { this.props.filterVenues(e.target.value) }} />
                 <br />
                 {
                     this.props.filteredVenues && this.props.filteredVenues.length > 0 && this.props.filteredVenues.map((venue, index) => (
